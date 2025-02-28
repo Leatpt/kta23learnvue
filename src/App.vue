d5994f9ad0e88c7f9195da282c06990655953d41
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-02-28 13:42:21 +0200

    Add Modal and ImageModal component

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,50 @@
 <script setup>
-import { ref, computed } from "vue";
-import ItemList from "./ItemList.vue";
-let message = ref('');
-let i = 0;
-let items = ref([
-  {id: i++, value: 'Piim', isDone: false },
-  {id: i++, value: 'Sai', isDone: true },
-  {id: i++, value: 'Viin', isDone: false },
-  {id: i++, value: 'Õlu', isDone: true },
-]);
-
-let doneItems = computed(() => items.value.filter(item => item.isDone));
-let toDoItems = computed(() => items.value.filter(item => !item.isDone));
-
-function addItem() {
-  if(message.value.trim() !== ''){
-    items.value.push({id: i++, value: message.value.trim(), isDone: false });
-  }
-  message.value = '';
-}
-
-
+import { ref } from 'vue';
+import Modal from './components/Modal.vue';
+import ImageModal from './components/ImageModal.vue';
+let modalActive = ref(false);
+let modalActive2 = ref(false);
+let modalActive3 = ref(false);
 </script>
 
 <template>
-  <div class="container content section">
-    <div class="field has-addons">
-      <div class="control is-expanded">
-        <input class="input" type="text" v-model="message" @keydown.enter="addItem">
-      </div>
-      <div class="control">
-        <button class="button is-primary" @click="addItem">
-          Add item
-        </button>
-      </div>
+    <div class="container content section">
+        <div class="buttons">
+            <button class="button is-primary" @click="modalActive = true">Open Modal</button>
+            <button class="button is-primary" @click="modalActive2 = true">Open Modal2</button>
+            <button class="button is-primary" @click="modalActive3 = true">Open Modal3</button>
+        </div>
     </div>
+    <ImageModal :active="modalActive" @close="modalActive = false" url="https://picsum.photos/seed/kaspar/1280/960"></ImageModal>
+    <ImageModal :active="modalActive2" @close="modalActive2 = false" url="https://picsum.photos/seed/martin/1280/960"></ImageModal>
+    <Modal :active="modalActive3" @close="modalActive3 = false">
+        <div class="card">
+            <div class="card-image">
+                <figure class="image is-4by3">
+                    <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="Placeholder image" />
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-left">
+                        <figure class="image is-48x48">
+                            <img src="https://bulma.io/assets/images/placeholders/96x96.png" alt="Placeholder image" />
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-4">John Smith</p>
+                        <p class="subtitle is-6">@johnsmith</p>
+                    </div>
+                </div>
 
-    <ItemList :items="items" title="All items"></ItemList>
-    <ItemList :items="doneItems" title="Done items"></ItemList>
-    <ItemList :items="toDoItems" title="ToDo items"></ItemList>
-
-  </div>
+                <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+                    iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+                    <a href="#">#responsive</a>
+                    <br />
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+            </div>
+        </div>
+    </Modal>
 </template>
-
-<style scoped></style>
